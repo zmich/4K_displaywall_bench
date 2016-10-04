efd8ed5d5457d43cad373f479cd0d0efbf25666f
cmanning <cmanning@oblong.com>
2016-10-04 12:49:00 -0700

    license update

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 
-Building:
+# 4K Displaywall Bench
+
+While most benchmarks are intended for the gaming industry, this simple app is for testing fillrate-bound applications such as those that need to drive high resolution displays, or many displays, or both.
+
+#Building
 
 cmake .
-
 make
 
+Use cmake flag -DCMAKE_BUILD_TYPE=Debug to enable OpenGL debug output extension (4.3 and higher)
+
+
+#Running
+
+./4K_displaywall_bench -help
 ./4K_displaywall_bench -width 11520 -height 2160 -swap_interval 0
 
+Use the width and height to define the viewport, which may span multiple displays.
+If your driver does not support OpenGL 4.4 (GLXBadFBConfig), run with -gl 3 for a version 3.3 context.
 
+#Troubleshooting
 
 you may need to do this:
 sudo wget -P /usr/include/GL http://www.opengl.org/registry/api/GL/glcorearb.h
@@ -17,6 +29,7 @@ sudo apt-get install libxrandr-dev
 sudo apt-get install libxinerama-dev
 sudo apt-get install libxcursor-dev
 
-If your driver does not support OpenGL 4.4 (GLXBadFBConfig), run with -gl 3 for a version 3.3 context.
 
+#License
 
+4K_displaywall_bench is freely available for download under the terms of the [BSD3]((https://github.com/Oblong/4K_displaywall_bench/blob/master/COPYING "COPYING") License.
--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
-
+/* 4K Displaywall Bench
+ * Copyright (c) 2016, Oblong Industries, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <Scene.h>
 
@@ -8,8 +37,8 @@
 #define GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB 0x8242
 
 namespace proto {
-  int width = 640;
-  int height = 480;
+  int width = 11520;
+  int height = 2160;
   int swap_interval = 0;
   int gl_version = 4;
   std::string filedir;
@@ -30,9 +59,9 @@ void help_msg (std::string prog)
 
 void validate_args ()
 {
-  if (width < 0 || width > 16384)
+  if (width < 0 || width > 32767)
     width = 640;
-  if (height < 0 || height > 16384)
+  if (height < 0 || height > 32767)
     height = 480;
   if (!(swap_interval == 0 || swap_interval == 1))
     swap_interval = 0;
